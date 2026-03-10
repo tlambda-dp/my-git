565624bf9436fcc88c5201db301b4211c102b44c
Lakshmi Prasanna Pamarthi <lakshmi.pamarthi@leadingtorch.com>
2026-03-10 16:59:09 +0530

    main.tf file updated

--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    local = {
-      source = "hashicorp/local"
-      version = "~> 2.0"
-    }
-  }
-}
-
-provider "local" {}
-
 resource "local_file" "example_file" {
-  content  = var.file_content
+  content  = "Terraform DevOps Practice"
   filename = var.file_name
 }
